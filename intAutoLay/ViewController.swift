6788f633fa46651ff57b8d9156a30f5e772dc281
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-11-01 15:26:32 -0700

    1st

--- intAutoLay/ViewController.swift
+++ intAutoLay/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+            // code
+            
+        
     }
 
+    
 
+    @IBAction func rotate(_ sender: Any) {
+        
+        
+      
+            
+            if stackView.axis == .horizontal {
+                stackView.axis = .vertical
+            }else {
+                stackView.axis = .horizontal
+            }
+        }
+        
+        
+        
+   
+    
+    
+    
 }
 
